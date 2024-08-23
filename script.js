7ff38d44eb11cd0e033057fd76b2b66e56fdde70
craftycheshire <mini_vann@ymail.com>
2024-08-22 19:07:59 -0500

    script.js

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
 let state = {
-    name: "",
+    firstName: "",
+    lastName: "",
     petName: "",
     zipCode: "",
+    phoneNumber: "",
+    email: "",
+    city: "",
+
 };
 
 let saveBtn = document.getElementById("saveButton");
-let nameInput = document.getElementById("firstName");
+let firstNameInput = document.getElementById("firstName");
+let lastNameInput = document.getElementById("lastName");
 let petNameInput = document.getElementById("petName");
 let zipCodeInput = document.getElementById("zipCode");
+let phoneNumberInput = document.getElementById("phoneNumber");
+let emailInput = document.getElementById("email");
+let cityInput = document.getElementById("city");
 
 function handleSave() {
     console.log("save button was clicked");
-    let name = nameInput.value;
+    let firstName = firstNameInput.value;
     state.firstName = firstName;
     console.log("Added name" + state.firstName);
 
     state.petName = petNameInput.value;
+    console.log("Added pet's name" + state.petName);
+
+    state.lastName = lastNameInput.value;
+    console.log("Added last name" + state.lastName);
+
     state.zipCode = zipCodeInput.value;
+    console.log("Added zip code" + state.zipCode);
+
+    state.phoneNumber = phoneNumberInput.value;
+    console.log("Added phone number" + state.phoneNumber);
+
+    state.email = emailInput.value;
+    console.log("Added email address" + state.email);
+
+    state.city = cityInput.value;
+    console.log("Added city" + state.city);
+
 
     console.log(state);
 
-    nameInput.value = "";
+    firstNameInput.value = "";
+    petNameInput.value = "";
+    lastNameInput.value = "";
     petNameInput.value = "";
     zipCodeInput.value = "";
 
-    let user = createFamily(state.name, state.petName, state.zipCode);
+    let user = createFamily(state.firstName, state.petName, state.zipCode);
     user.status();
     user.render();
 }
